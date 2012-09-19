952edf1b48ce5613e52eb15bf9e335756da7931b
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2012-09-19 13:17:47 +0200

    starting/stopping ejb container before/after class, not test

--- src/test/java/net/kaczmarzyk/jpacert/test/EjbContainerTestBase.java
+++ src/test/java/net/kaczmarzyk/jpacert/test/EjbContainerTestBase.java
@@ -7,8 +7,8 @@ import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
 import javax.naming.NamingException;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 
 public abstract class EjbContainerTestBase {
@@ -16,11 +16,11 @@ public abstract class EjbContainerTestBase {
 	private static final Object APPLICATION_NAME = "JpaCertApp";
 	private static final String MODULE_NAME = "EjbModule";
 	
-	protected Context context;
-	private EJBContainer container;
+	protected static Context context;
+	private static EJBContainer container;
 	
-	@Before
-	public void createContainer () {
+	@BeforeClass
+	public static void createContainer () {
 		Map<String, Object> properties = new HashMap<String, Object>();
 		properties.put(EJBContainer.APP_NAME, APPLICATION_NAME);
 		
@@ -28,8 +28,8 @@ public abstract class EjbContainerTestBase {
 		context = container.getContext();
 	}
 	
-	@After
-	public void closeContainer() {
+	@AfterClass
+	public static void closeContainer() {
 		container.close();
 	}
 	
