a7287b9a70df476dbf77544caac14484319ec103
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2012-10-28 11:14:59 +0100

    compound primary key (@IdClass) example

--- src/main/java/net/kaczmarzyk/jpacert/domain/Project.java
+++ src/main/java/net/kaczmarzyk/jpacert/domain/Project.java
@@ -5,18 +5,24 @@ import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
 
 @Entity
+@IdClass(ProjectKey.class)
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="project_type")
 public abstract class Project {
 
 	@Id @GeneratedValue
 	private Long id;
+	
+	@Id
+	private String code = "default";
+	
 	
 	@Column(unique=true)
 	private String name;
---
+++ src/main/java/net/kaczmarzyk/jpacert/domain/ProjectKey.java
@@ -0,0 +1,51 @@
+package net.kaczmarzyk.jpacert.domain;
+
+import java.io.Serializable;
+
+
+public class ProjectKey implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String code;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getCode() {
+		return code;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProjectKey other = (ProjectKey) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
